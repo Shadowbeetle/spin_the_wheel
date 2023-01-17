1a238692a605a5a12dc73c91907223a69235b6cb
Shadowbeetle <tamas.kadlecsik@gmail.com>
2023-01-17 15:20:11 +0100

    reduce default pool size

--- config/runtime.exs
+++ config/runtime.exs
@@ -34,7 +34,7 @@ if config_env() == :prod do
   config :spin_the_wheel, SpinTheWheel.Repo,
     # ssl: true,
     url: database_url,
-    pool_size: String.to_integer(System.get_env("POOL_SIZE") || "3"),
+    pool_size: String.to_integer(System.get_env("POOL_SIZE") || "2"),
     socket_options: maybe_ipv6
 
   # The secret key base is used to sign/encrypt cookies and other secrets.
